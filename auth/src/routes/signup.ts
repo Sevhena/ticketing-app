ff9db87570f481150776c2603f97492a2ee24fe4
Sevhena Walker <83547364+Sevhena@users.noreply.github.com>
2024-07-28 10:05:00 -0400

    Moved application cluster to gcloud

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -22,10 +23,17 @@ router
         throw new RequestValidationError(errors.array());
       }
 
-      console.log('Creating a user...');
-      throw new DatabaseConnectionError();
+      const { email, password } = req.body;
+      const existingUser = await User.findOne({ email });
 
-      res.send({});
+      if (existingUser) {
+        throw new BadRequestError('Email in use');
+      }
+
+      const user = User.build({ email, password });
+      await user.save();
+
+      res.status(201).json({ status: 'success', user });
     }
   );
 

c52bfa2520e0c9894ddfa1a031e8d3e8dc9b60d2
Sevhena Walker <83547364+Sevhena@users.noreply.github.com>
2024-08-29 10:50:00 -0400

    Last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it('responds with details about the current user', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(400);
 
   console.log(response.body);
 
